9dcfd1145c1111c901aa18d167b28caea5ea28ef
faima-ahmed <faimaahmeddiya@gmail.com>
2024-07-21 11:12:11 +0600

    Commit 1

--- script.js
+++ script.js
@@ -54,9 +54,10 @@ Array.prototype.unique= function(){
 console.log(arr.unique());
 */
 //es6 classes
+
 class PersonCl{
-  constructor(firstName, birthYear){
-     this.firstName = firstName;
+  constructor(fullName, birthYear){
+     this.fullName = fullName;
      this.birthYear = birthYear;
   }
 calcAge(){
@@ -66,11 +67,27 @@ calcAge(){
 greet(){
   console.log(`Hey ${this.firstName}`);
 }
+
+get age(){
+  return 2037-this.birthYear;
+}
+
+set fullName(name){
+  console.log(name);
+  if(name.includes(' ')) this._fullName=name;
+  else alert(`${name} is not a full name!`);
+}
+
+get fullName(){
+  return this._fullName;
+}
+
 }  
 
-const jessica= new PersonCl('Jessica', 1996);
+const jessica= new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -79,3 +96,24 @@ console.log(jessica.__proto__ === PersonCl.prototype);
 // }
 
 jessica.greet();
+
+const faima = new PersonCl('Faima Ahmed', 1965);
+
+ 
+const account={
+  owner:'Faima',
+  movements: [200, 530, 120, 300],
+
+  get latest(){
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov){
+    this.movements.push(mov);
+  }
+}
+
+console.log(account.latest);
+
+account.latest=50;
+console.log(account.movements);
