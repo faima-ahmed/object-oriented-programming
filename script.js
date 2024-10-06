8b96aad92d02da2afa065a0f77d69849277f332e
faima-ahmed <faimaahmeddiya@gmail.com>
2024-10-06 16:37:59 +0600

    coading-challenge

--- script.js
+++ script.js
@@ -59,8 +59,23 @@ class PersonCl{
      this.firstName = firstName;
      this.birthYear = birthYear;
   }
+calcAge(){
+  console.log(2037-this.birthYear);
+}
 
+greet(){
+  console.log(`Hey ${this.firstName}`);
+}
 }  
 
 const jessica= new PersonCl('Jessica', 1996);
 console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet= function(){
+//   console.log(`Hey ${this.firstName}`);
+// }
+
+jessica.greet();
